07c50d75be193e43f69965f5d7ee73e0d5aeef35
mohsen104 <m.karimvand.84@gmail.com>
2026-09-03 20:15:13 +0330

    install next new

--- next.config.ts
+++ next.config.ts
@@ -2,21 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactStrictMode: true,
-  compress: true,
-  poweredByHeader: false,
-  productionBrowserSourceMaps: false,
-  experimental: {
-    turbopackMinify: true,
-    scrollRestoration: false,
-    optimizePackageImports: ["swiper"],
-  },
-  images: {
-    formats: ["image/avif", "image/webp"],
-  },
-  compiler: {
-    removeConsole: process.env.NODE_ENV === "production",
-  },
 };
 
 export default nextConfig;
